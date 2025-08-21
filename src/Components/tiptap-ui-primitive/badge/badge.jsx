cbf830845747013e8fbe31724512c2c683516ad2
Ahsan2341 <dev.ahsanrehman@irsolutions.tech>
2025-08-21 11:56:31 +0500

    all components captialized

--- src/Components/tiptap-ui-primitive/badge/badge.jsx
+++ src/Components/tiptap-ui-primitive/badge/badge.jsx
@@ -1,34 +1,37 @@
-import * as React from "react"
-import "@/components/tiptap-ui-primitive/badge/badge-colors.scss"
-import "@/components/tiptap-ui-primitive/badge/badge-group.scss"
-import "@/components/tiptap-ui-primitive/badge/badge.scss"
+import * as React from "react";
+import "@/Components/tiptap-ui-primitive/badge/badge-colors.scss";
+import "@/Components/tiptap-ui-primitive/badge/badge-group.scss";
+import "@/Components/tiptap-ui-primitive/badge/badge.scss";
 
-export const Badge = React.forwardRef((
-  {
-    variant,
-    size = "default",
-    appearance = "default",
-    trimText = false,
-    className,
-    children,
-    ...props
-  },
-  ref
-) => {
-  return (
-    <div
-      ref={ref}
-      className={`tiptap-badge ${className || ""}`}
-      data-style={variant}
-      data-size={size}
-      data-appearance={appearance}
-      data-text-trim={trimText ? "on" : "off"}
-      {...props}>
-      {children}
-    </div>
-  );
-})
+export const Badge = React.forwardRef(
+  (
+    {
+      variant,
+      size = "default",
+      appearance = "default",
+      trimText = false,
+      className,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div
+        ref={ref}
+        className={`tiptap-badge ${className || ""}`}
+        data-style={variant}
+        data-size={size}
+        data-appearance={appearance}
+        data-text-trim={trimText ? "on" : "off"}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  }
+);
 
-Badge.displayName = "Badge"
+Badge.displayName = "Badge";
 
-export default Badge
+export default Badge;
