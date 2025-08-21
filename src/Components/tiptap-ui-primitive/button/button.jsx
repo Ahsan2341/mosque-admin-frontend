8b56d99f8793665a3edbeb060e254f77095debaf
Ahsan2341 <dev.ahsanrehman@irsolutions.tech>
2025-08-21 11:55:28 +0500

    all components captialized

--- src/Components/tiptap-ui-primitive/button/button.jsx
+++ src/Components/tiptap-ui-primitive/button/button.jsx
@@ -1,23 +1,21 @@
-import * as React from "react"
+import * as React from "react";
 
 // --- Tiptap UI Primitive ---
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from "@/components/tiptap-ui-primitive/tooltip"
+} from "@/Components/tiptap-ui-primitive/tooltip";
 
 // --- Lib ---
-import { cn, parseShortcutKeys } from "@/lib/tiptap-utils"
+import { cn, parseShortcutKeys } from "@/lib/tiptap-utils";
 
-import "@/components/tiptap-ui-primitive/button/button-colors.scss"
-import "@/components/tiptap-ui-primitive/button/button-group.scss"
-import "@/components/tiptap-ui-primitive/button/button.scss"
+import "@/Components/tiptap-ui-primitive/button/button-colors.scss";
+import "@/Components/tiptap-ui-primitive/button/button-group.scss";
+import "@/Components/tiptap-ui-primitive/button/button.scss";
 
-export const ShortcutDisplay = ({
-  shortcuts,
-}) => {
-  if (shortcuts.length === 0) return null
+export const ShortcutDisplay = ({ shortcuts }) => {
+  if (shortcuts.length === 0) return null;
 
   return (
     <div>
@@ -29,65 +27,75 @@ export const ShortcutDisplay = ({
       ))}
     </div>
   );
-}
+};
 
-export const Button = React.forwardRef((
-  {
-    className,
-    children,
-    tooltip,
-    showTooltip = true,
-    shortcutKeys,
-    "aria-label": ariaLabel,
-    ...props
-  },
-  ref
-) => {
-  const shortcuts = React.useMemo(() => parseShortcutKeys({ shortcutKeys }), [shortcutKeys])
+export const Button = React.forwardRef(
+  (
+    {
+      className,
+      children,
+      tooltip,
+      showTooltip = true,
+      shortcutKeys,
+      "aria-label": ariaLabel,
+      ...props
+    },
+    ref
+  ) => {
+    const shortcuts = React.useMemo(
+      () => parseShortcutKeys({ shortcutKeys }),
+      [shortcutKeys]
+    );
 
-  if (!tooltip || !showTooltip) {
+    if (!tooltip || !showTooltip) {
+      return (
+        <button
+          className={cn("tiptap-button", className)}
+          ref={ref}
+          aria-label={ariaLabel}
+          {...props}
+        >
+          {children}
+        </button>
+      );
+    }
+
     return (
-      <button
-        className={cn("tiptap-button", className)}
-        ref={ref}
-        aria-label={ariaLabel}
-        {...props}>
-        {children}
-      </button>
+      <Tooltip delay={200}>
+        <TooltipTrigger
+          className={cn("tiptap-button", className)}
+          ref={ref}
+          aria-label={ariaLabel}
+          {...props}
+        >
+          {children}
+        </TooltipTrigger>
+        <TooltipContent>
+          {tooltip}
+          <ShortcutDisplay shortcuts={shortcuts} />
+        </TooltipContent>
+      </Tooltip>
     );
   }
+);
 
-  return (
-    <Tooltip delay={200}>
-      <TooltipTrigger
-        className={cn("tiptap-button", className)}
+Button.displayName = "Button";
+
+export const ButtonGroup = React.forwardRef(
+  ({ className, children, orientation = "vertical", ...props }, ref) => {
+    return (
+      <div
         ref={ref}
-        aria-label={ariaLabel}
-        {...props}>
+        className={cn("tiptap-button-group", className)}
+        data-orientation={orientation}
+        role="group"
+        {...props}
+      >
         {children}
-      </TooltipTrigger>
-      <TooltipContent>
-        {tooltip}
-        <ShortcutDisplay shortcuts={shortcuts} />
-      </TooltipContent>
-    </Tooltip>
-  );
-})
+      </div>
+    );
+  }
+);
+ButtonGroup.displayName = "ButtonGroup";
 
-Button.displayName = "Button"
-
-export const ButtonGroup = React.forwardRef(({ className, children, orientation = "vertical", ...props }, ref) => {
-  return (
-    <div
-      ref={ref}
-      className={cn("tiptap-button-group", className)}
-      data-orientation={orientation}
-      role="group"
-      {...props}>
-      {children}
-    </div>
-  );
-})
-ButtonGroup.displayName = "ButtonGroup"
-
-export default Button
+export default Button;
--- src/Components/tiptap-ui-primitive/card/card.jsx
+++ src/Components/tiptap-ui-primitive/card/card.jsx
@@ -1,43 +1,65 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { cn } from "@/lib/tiptap-utils"
-import "@/components/tiptap-ui-primitive/card/card.scss"
+import * as React from "react";
+import { cn } from "@/lib/tiptap-utils";
+import "@/Components/tiptap-ui-primitive/card/card.scss";
 
 const Card = React.forwardRef(({ className, ...props }, ref) => {
   return <div ref={ref} className={cn("tiptap-card", className)} {...props} />;
-})
-Card.displayName = "Card"
+});
+Card.displayName = "Card";
 
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => {
-  return (<div ref={ref} className={cn("tiptap-card-header", className)} {...props} />);
-})
-CardHeader.displayName = "CardHeader"
+  return (
+    <div ref={ref} className={cn("tiptap-card-header", className)} {...props} />
+  );
+});
+CardHeader.displayName = "CardHeader";
 
 const CardBody = React.forwardRef(({ className, ...props }, ref) => {
-  return (<div ref={ref} className={cn("tiptap-card-body", className)} {...props} />);
-})
-CardBody.displayName = "CardBody"
+  return (
+    <div ref={ref} className={cn("tiptap-card-body", className)} {...props} />
+  );
+});
+CardBody.displayName = "CardBody";
 
-const CardItemGroup = React.forwardRef(({ className, orientation = "vertical", ...props }, ref) => {
+const CardItemGroup = React.forwardRef(
+  ({ className, orientation = "vertical", ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        data-orientation={orientation}
+        className={cn("tiptap-card-item-group", className)}
+        {...props}
+      />
+    );
+  }
+);
+CardItemGroup.displayName = "CardItemGroup";
+
+const CardGroupLabel = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <div
       ref={ref}
-      data-orientation={orientation}
-      className={cn("tiptap-card-item-group", className)}
-      {...props} />
+      className={cn("tiptap-card-group-label", className)}
+      {...props}
+    />
   );
-})
-CardItemGroup.displayName = "CardItemGroup"
-
-const CardGroupLabel = React.forwardRef(({ className, ...props }, ref) => {
-  return (<div ref={ref} className={cn("tiptap-card-group-label", className)} {...props} />);
-})
-CardGroupLabel.displayName = "CardGroupLabel"
+});
+CardGroupLabel.displayName = "CardGroupLabel";
 
 const CardFooter = React.forwardRef(({ className, ...props }, ref) => {
-  return (<div ref={ref} className={cn("tiptap-card-footer", className)} {...props} />);
-})
-CardFooter.displayName = "CardFooter"
+  return (
+    <div ref={ref} className={cn("tiptap-card-footer", className)} {...props} />
+  );
+});
+CardFooter.displayName = "CardFooter";
 
-export { Card, CardHeader, CardFooter, CardBody, CardItemGroup, CardGroupLabel }
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardBody,
+  CardItemGroup,
+  CardGroupLabel,
+};
